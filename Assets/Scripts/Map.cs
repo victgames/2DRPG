591ac3768b6c344e53247469856f708623edc280
victgames-admin <victgames777@gmail.com>
2025-01-04 22:44:30 +0900

    playerを動かすところまで実施 Player動作しないためでバックから

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,52 @@
+//Map.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Map : MonoBehaviour
+{
+    public Grid Grid { get => GetComponent<Grid>(); }
+    Dictionary<string, Tilemap> _tilemaps;
+
+    readonly static string BACKGROND_TILEMAP_NAME = "Background";
+    readonly static string NONE_OBJECTS_TILEMAP_NAME = "NoneObjects";
+    readonly static string OBJECTS_TILEMAP_NAME = "Objects";
+    readonly static string EVENT_BOX_TILEMAP_NAME = "EventBox";
+
+    private void Awake()
+    {
+        _tilemaps = new Dictionary<string, Tilemap>();
+        foreach (var tilemap in Grid.GetComponentsInChildren<Tilemap>())
+        {
+            _tilemaps.Add(tilemap.name, tilemap);
+        }
+    }
+
+    public Vector3 GetWorldPos(Vector3Int pos)
+    {
+        return Grid.CellToWorld(pos);
+    }
+
+    public class Mass
+    {
+        public bool isMovable;
+        public TileBase eventTile;
+    }
+    public Mass GetMassData(Vector3Int pos)
+    {
+        var mass = new Mass();
+        mass.eventTile = _tilemaps[EVENT_BOX_TILEMAP_NAME].GetTile(pos);
+        mass.isMovable = true;
+
+        if (_tilemaps[OBJECTS_TILEMAP_NAME].GetTile(pos))
+        {
+            mass.isMovable = false;
+        }
+        else if (_tilemaps[BACKGROND_TILEMAP_NAME].GetTile(pos) == null)
+        {
+            mass.isMovable = false;
+        }
+        return mass;
+    }
+}
